0d77c8fb15ba2668f425770477b8401b286c5f64
John Fiocca <johnfiocca@Johns-MacBook-Pro-2.local>
2024-08-15 21:39:24 -0400

    a few movements and edits of the core file

--- src/chucks/__init__.py
+++ src/chucks/__init__.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2024-present John Fiocca <fioccajohn@gmail.com>
 from .core import ChucksAccessor
 
-__all__ = ['ChucksAccessor']
+__all__ = ["ChucksAccessor"]
--- src/chucks/core.py
+++ src/chucks/core.py
@@ -1,15 +1,17 @@
 # coding: utf-8
 import json
 from pathlib import Path
+
 import pandas as pd
+from schwab.auth import easy_client
 
-CONFIG_FILE = Path('/Users/johnfiocca/.config/chucks/config.json')
+# TODO: Could this work as a massive wrapper around the schwab-py package?
 
-with open(CONFIG_FILE) as f:
-            j = json.load(f)
 
 @pd.api.extensions.register_dataframe_accessor("chucks")
 class ChucksAccessor:
+
+
     def __init__(self, pandas_obj):
         self._validate(pandas_obj)
         self._obj = pandas_obj
@@ -17,22 +19,43 @@ class ChucksAccessor:
         self.another_namespace = NamespaceClassForLikeQuote
 
     @staticmethod
-    def _validate(obj):
+    def _validate(response_obj):
+        # TODO: what's the input here?
+        # TODO
         pass
 
     @staticmethod
-    def from_candles(obj):
-        return pd.json_normalize(obj, record_path='candles')
+    def from_price_history(response_obj):
+        # TODO
+        return pd.json_normalize(response_obj, record_path="candles")
+
+    @staticmethod
+    def from_XXX(response_obj):
+        # TODO
+        pass
 
     @property
-    def price_history_dataframe(self):
+    def something(self):
+        # TODO
         pass
 
     def plot(self):
+        # TODO
         pass
 
-if __name__ == "__main__":
-    c = easy_client(j.get('client_id'), j.get('client_secret'), j.get('redirect_uri'), Path('/Users/johnfiocca/.config/chucks/access_token.json'))
-    r = c.get_price_history('SPY')
-    df = pd.json_normalize(r.json())
 
+if __name__ == "__main__":
+
+    CONFIG_FILE = Path("/Users/johnfiocca/.config/chucks/config.json")
+
+    with open(CONFIG_FILE) as f:
+        j = json.load(f)
+
+    c = easy_client(
+        j.get("client_id"),
+        j.get("client_secret"),
+        j.get("redirect_uri"),
+        Path("/Users/johnfiocca/.config/chucks/access_token.json"),
+    )
+    r = c.get_price_history("SPY")
+    df = pd.DataFrame.chucks.from_price_history(r)
