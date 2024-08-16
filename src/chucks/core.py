3ff5270af9a8445f832234a95b17661e3b1ad218
John Fiocca <johnfiocca@Johns-MacBook-Pro-2.local>
2024-08-16 19:35:06 -0400

    Modifying the design. Thinking about how to couple. schwab-api within? Maybe not.

--- src/chucks/core.py
+++ src/chucks/core.py
@@ -1,22 +1,24 @@
 # coding: utf-8
+from datetime import datetime, timedelta
 import json
 from pathlib import Path
-
 import pandas as pd
 from schwab.auth import easy_client
 
 # TODO: Could this work as a massive wrapper around the schwab-py package?
 
+def token_creation_date(schwab_client):
+    """Get token creation date."""
+    # TODO is this access or refresh? presumably refresh token creation.
+    return datetime.ctime(datetime.now() - timedelta(seconds=schwab_client.token_age()))
 
 @pd.api.extensions.register_dataframe_accessor("chucks")
 class ChucksAccessor:
-
-
     def __init__(self, pandas_obj):
         self._validate(pandas_obj)
         self._obj = pandas_obj
-        self.some_other_namespace = NamespaceClassForLikePriceHistory
-        self.another_namespace = NamespaceClassForLikeQuote
+        # self.some_other_namespace = NamespaceClassForLikePriceHistory
+        # self.another_namespace = NamespaceClassForLikeQuote
 
     @staticmethod
     def _validate(response_obj):
@@ -27,7 +29,7 @@ class ChucksAccessor:
     @staticmethod
     def from_price_history(response_obj):
         # TODO
-        return pd.json_normalize(response_obj, record_path="candles")
+        return pd.json_normalize(response_obj.json(), record_path="candles", meta=['symbol', 'empty'])
 
     @staticmethod
     def from_XXX(response_obj):
@@ -35,7 +37,11 @@ class ChucksAccessor:
         pass
 
     @property
-    def something(self):
+    def some_property(self):
+        # TODO
+        pass
+
+    def some_method(self):
         # TODO
         pass
 
